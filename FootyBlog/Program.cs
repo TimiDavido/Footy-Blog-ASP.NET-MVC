d1fb3c5b329f21a0b73906cb69f1e591fdf86eef
Opeyemi David <opeyemidavid@globusbank.com>
2026-08-17 09:40:07 +0100

    registered blog services with DI

--- FootyBlog/Program.cs
+++ FootyBlog/Program.cs
@@ -1,5 +1,11 @@
+using FootyBlog.Models;
+using FootyBlog.ServiceContracts;
+using FootyBlog.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<BlogRepository>();
+builder.Services.AddScoped<IBlogService, BlogService>();
 
 var app = builder.Build();
 
